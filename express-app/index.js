d9d85dc148705c94ebbf749b556b815347c486dc
Miika Oinonen <mhtoin@gmail.com>
2023-10-03 12:34:21 +0300

    change

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello there little guy! Nice to see you! Hope this works.</h1>')
+  res.send('<h1>Hello there!</h1>')
 })
 
 const PORT = 8080
